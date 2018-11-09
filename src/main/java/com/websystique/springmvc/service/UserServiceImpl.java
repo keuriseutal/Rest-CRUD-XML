8cd2c9c230c2dc0f824f56de3b0c18cce4099733
crystaljoy.tolentino@gmail.com <crystaljoy.tolentino@gmail.com>
2018-11-09 09:42:46 +0800

    first commit

---
+++ src/main/java/com/websystique/springmvc/service/UserServiceImpl.java
@@ -0,0 +1,87 @@
+package com.websystique.springmvc.service;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.websystique.springmvc.model.User;
+
+@Service("userService")
+@Transactional
+public class UserServiceImpl implements UserService {
+
+	private static final AtomicLong counter = new AtomicLong();
+
+	private static List<User> users;
+
+	static {
+		users = populateDummyUsers();
+	}
+
+	public List<User> findAllUsers() {
+		return users;
+	}
+
+	public User findById(long id) {
+		for (User user : users) {
+			if (user.getId() == id) {
+				return user;
+			}
+		}
+		return null;
+	}
+
+	public User findByName(String name) {
+		for (User user : users) {
+			if (user.getName().equalsIgnoreCase(name)) {
+				return user;
+			}
+		}
+		return null;
+	}
+
+	public void saveUser(User user) {
+		user.setId(counter.incrementAndGet());
+		users.add(user);
+	}
+
+	public void updateUser(User user) {
+		int index = users.indexOf(user);
+		users.set(index, user);
+	}
+
+	public void deleteUserById(long id) {
+
+		for (Iterator<User> iterator = users.iterator(); iterator.hasNext();) {
+			User user = iterator.next();
+			if (user.getId() == id) {
+				iterator.remove();
+			}
+		}
+	}
+
+	public boolean isUserExist(User user) {
+		return findByName(user.getName()) != null;
+	}
+
+	private static List<User> populateDummyUsers() {
+		List<User> users = new ArrayList<User>();
+		users.add(new User(counter.incrementAndGet(), "Crystal", 20, 1000000));
+		users.add(new User(counter.incrementAndGet(), "Danielle", 20, 500000));
+		users.add(new User(counter.incrementAndGet(), "Joohyun", 27, 250000));
+		users.add(new User(counter.incrementAndGet(), "Seulgi", 24, 125000));
+		users.add(new User(counter.incrementAndGet(), "Jisoo", 23, 62500));
+		users.add(new User(counter.incrementAndGet(), "Jennie", 22, 31250));
+
+		return users;
+	}
+
+	public void deleteAllUsers() {
+		users.clear();
+	}
+
+}
